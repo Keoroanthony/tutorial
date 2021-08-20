4760d006c3dfbd8ef298c2ab53ce59792062d954
tony <anthony.keoro@gmail.com>
2021-08-20 09:48:44 +0300

    added

---
+++ Ternary.java
@@ -0,0 +1,11 @@
+public class Ternary {
+        public static void main(String[] args) {
+
+            int februaryDays = 29;
+            String result;
+
+            // ternary operator
+            result = (februaryDays == 28) ? "Leap year" : "Not a leap year";
+            System.out.println(result);
+        }
+}
